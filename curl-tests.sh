9ac22a529f9dca315f8b8e49710bcca2d0f84f39
sevren <kgoguev@gmail.com>
2019-04-19 13:27:53 +0200

    added readme for license manager, fix curl tests, added right command for pairing-manager

--- curl-tests.sh
+++ curl-tests.sh
@@ -1,15 +1,25 @@
-#!/bin/bash
+#!/bin/sh
 
 #The following script file will hit the diffrent endpoints 
-
-docker-compose up -d
-
+docker-compose up -d rabbitmq
 # wait for everything to have been started.. 
 sleep 5
+# Start the rest
+docker-compose up -d license-manager
+sleep 2
+docker-compose up -d pairing-manager
 
-curl -X POST -d "{\"password\":\"s3cr3t\"}" http://localhost:8080/lisa
+echo "\n\n Sending a POST request for user lisa\n\n"
+curl -v -d "{\"password\":\"s3cr3t\"}" http://localhost:8080/lisa
 
-curl -X POST -d "{\"password\":\"s3cr3t\"}" http://localhost:8080/lisa/licenses
+echo "\n\nSending a POST request for user lisa with the correct password \n\n"
+curl -v  -d "{\"password\":\"s3cr3t\"}" http://localhost:8080/lisa/licenses
 
-curl -X POST -d "{\"password\":\"qwerty\"}" http://localhost:8080/john/licenses
+echo "\n\nSending a POST request for user lisa with the wrong password \n\n"
+curl -v  -d "{\"password\":\"wrong-password\"}" http://localhost:8080/lisa/licenses
 
+echo "\n\nSending a POST request for user john with the correct password \n\n"
+curl -v -d "{\"password\":\"qwerty\"}" http://localhost:8080/john/licenses
+
+echo "\n\nSending a POST request for a nonexistant user \n\n"
+curl -v -d "{\"password\":\"cvxcv\"}" http://localhost:8080/omg-i-dont-exist/licenses
