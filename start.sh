ae8b7db95c613c4967d956c571ed0f44b76595c4
sevren <kgoguev@gmail.com>
2019-04-22 20:58:52 +0200

    fixed start.sh to sleep 10s after starting rabbitmq

--- start.sh
+++ start.sh
@@ -3,6 +3,10 @@
 #The following script file will hit the diffrent endpoints 
 docker-compose up -d rabbitmq
 # wait for everything to have been started.. 
-sleep 5
+echo "Sleeping for a bit, rabbitmq takes some time to start up :("
+sleep 10
 # Start the rest
-docker-compose up 
+docker-compose up -d license-manager
+sleep 2
+docker-compose up -d pairing-manager
+docker-compose logs -f license-manager pairing-manager
